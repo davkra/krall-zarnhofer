f86fe86e8005310ae1e999d18a4e9f4105c8d539
David Krall <davkra@chello.at>
2025-01-21 20:26:43 +0100

    added calculator web app, which uses python code internally

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,3 +1,14 @@
+import sys
+import json
+
+
+def calculate(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+
+
 def hello():
     return "Hello world!"
 
@@ -11,4 +22,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    data = json.loads(sys.stdin.read())
+    num1 = float(data["num1"])
+    num2 = float(data["num2"])
+    operation = data["operation"]
+    result = calculate(num1, num2, operation)
+    print(json.dumps({"result": result}))
