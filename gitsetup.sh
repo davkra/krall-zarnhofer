6f67d5197d19d54564838a00ef4e8a62446db07e
David Krall <davkra@chello.at>
2025-01-25 15:05:29 +0100

    fixed using correct remote fetch repository

--- gitsetup.sh
+++ gitsetup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 git remote remove origin
-git remote add origin git@git-iit.fh-joanneum.at:msd-contdel/techdemo-ws24/krall-zarnhofer.git
-
-# Mirror Repository
-git remote set-url --add --push origin git@git-iit.fh-joanneum.at:msd-contdel/techdemo-ws24/krall-zarnhofer.git
+git remote add origin git@github.com:davkra/krall-zarnhofer.git
 
 # Main Repository
 git remote set-url --add --push origin git@github.com:davkra/krall-zarnhofer.git
+
+# Mirror Repository
+git remote set-url --add --push origin git@git-iit.fh-joanneum.at:msd-contdel/techdemo-ws24/krall-zarnhofer.git
 
 git fetch
 
