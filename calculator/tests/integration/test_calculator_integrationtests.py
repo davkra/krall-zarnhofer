da75a708ad0784508366bafe3c7f27097d727d93
David Krall <davkra@chello.at>
2025-01-27 17:56:12 +0100

    added multiplication and division

--- calculator/tests/integration/test_calculator_integrationtests.py
+++ calculator/tests/integration/test_calculator_integrationtests.py
@@ -17,3 +17,17 @@ class TestStringManipulator(unittest.TestCase):
         self.assertEqual(calculator.calculate(3, 3, "subtract"), 0)
         self.assertEqual(calculator.calculate(2, 4, "subtract"), -2)
         self.assertEqual(calculator.calculate(1, 5, "subtract"), -4)
+
+    def test_multiplication(self):
+        self.assertEqual(calculator.calculate(1, 5, "multiply"), 5)
+        self.assertEqual(calculator.calculate(2, 4, "multiply"), 8)
+        self.assertEqual(calculator.calculate(3, 3, "multiply"), 9)
+        self.assertEqual(calculator.calculate(4, 2, "multiply"), 8)
+        self.assertEqual(calculator.calculate(5, 1, "multiply"), 5)
+
+    def test_division(self):
+        self.assertEqual(calculator.calculate(5, 1, "divide"), 5)
+        self.assertEqual(calculator.calculate(4, 1, "divide"), 4)
+        self.assertEqual(calculator.calculate(3, 1, "divide"), 3)
+        self.assertEqual(calculator.calculate(2, 1, "divide"), 2)
+        self.assertEqual(calculator.calculate(1, 1, "divide"), 1)
--- calculator/tests/unit/test_calculator_unittests.py
+++ calculator/tests/unit/test_calculator_unittests.py
@@ -21,3 +21,17 @@ class TestStringManipulator(unittest.TestCase):
         self.assertEqual(calculator.subtraction(3, 3), 0)
         self.assertEqual(calculator.subtraction(2, 4), -2)
         self.assertEqual(calculator.subtraction(1, 5), -4)
+
+    def test_multiplication(self):
+        self.assertEqual(calculator.multiplication(1, 5), 5)
+        self.assertEqual(calculator.multiplication(2, 4), 8)
+        self.assertEqual(calculator.multiplication(3, 3), 9)
+        self.assertEqual(calculator.multiplication(4, 2), 8)
+        self.assertEqual(calculator.multiplication(5, 1), 5)
+
+    def test_division(self):
+        self.assertEqual(calculator.division(5, 1), 5)
+        self.assertEqual(calculator.division(4, 1), 4)
+        self.assertEqual(calculator.division(3, 1), 3)
+        self.assertEqual(calculator.division(2, 1), 2)
+        self.assertEqual(calculator.division(1, 1), 1)
